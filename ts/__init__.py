8caa1ecb18796cc075ad58d7b009e77c0ec55213
tjacek <tjacek@agh.edu.pl>
2020-01-21 15:49:23 +0100

    ens extraction

--- ts/__init__.py
+++ ts/__init__.py
@@ -1,12 +1,32 @@
 import numpy as np
 import keras
-#import files,data,single,tools
-import single,data,norm
+from keras.models import load_model
+import single,data,norm,files
 import gen,ts.models
+
+def ens_extract(frame_path,model_path,out_path=None):  
+    files.make_dir(out_path)
+    for i,in_i in enumerate(files.top_files(model_path)):
+        out_i="%s/nn%d"%(out_path,i)
+        frame_i="%s/nn%d"%(frame_path,i)
+        extract(frame_i,in_i,out_i)
+
+def extract(frame_path,model_path,out_path):
+    (X,y),names=load_data(frame_path,split=False)
+    extractor=load_model(model_path)
+    X_feats=extractor.predict(X)
+    feat_dict={ names[i]:feat_i for i,feat_i in enumerate(X_feats)}
+    single.save_ts_feats(feat_dict,out_path)
+
+def ens_train(in_path,out_path,n_epochs=5):
+    files.make_dir(out_path)
+    for i,in_i in enumerate(files.top_files(in_path)):
+        out_i="%s/nn%d"%(out_path,i)
+        make_model(in_i,out_i,n_epochs)
 
 def make_model(in_path,out_path=None,n_epochs=5):
     (X_train,y_train),test,params=load_data(in_path,split=True)
-#    X_train=np.squeeze(X_train)
+    print(params['n_cats'])
     X,y=gen.full_data(X_train,y_train)
     sim_metric,model=ts.models.make_model(params)
     sim_metric.fit(X,y,epochs=n_epochs,batch_size=100)
@@ -15,11 +35,9 @@ def make_model(in_path,out_path=None,n_epochs=5):
 
 def load_data(in_path,split=True):   
     feat_dict=single.read_frame_feats(in_path)
-#    raise Exception(list(feat_dict.values())[0].shape)
     if(split):
         train,test=data.split(feat_dict.keys())
         train,test=prepare_data(train,feat_dict),prepare_data(test,feat_dict)
-#        raise Exception(train[0].shape)
         params={'ts_len':train[0].shape[1],'n_feats':train[0].shape[2],'n_cats': train[1].shape[1]}
         return train,test,params
     else:
@@ -29,7 +47,6 @@ def load_data(in_path,split=True):
 def prepare_data(names,feat_dict):
     X=np.array([feat_dict[name_i] for name_i in names])
     X=norm.normalize(X,'all')
-#    X=np.expand_dims(X,axis=-1)
     y=[data.parse_name(name_i)[0]-1 for name_i in names]
     y=keras.utils.to_categorical(y)
     return np.array(X),y
