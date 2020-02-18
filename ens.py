bc7f3872c885a804ac0a8b6a0190a2d7ecdff65a
tjacek <tjacek@agh.edu.pl>
2020-02-18 23:09:53 +0100

    add fun arg

--- ens.py
+++ ens.py
@@ -1,14 +1,16 @@
 import os.path
-import single,files
+import single,files,basic
 
-def frame_feats(frame_path,model_path,out_path=None):
+def frame_feats(frame_path,model_path,out_path=None,fun=None):
     if(not out_path):
         dst_dir=os.path.split(model_path)[0]
         out_path=dst_dir+'/seqs'	
     files.make_dir(out_path)
+    if(not fun):
+        fun= single.extract_frame_feats
     for i,in_i in enumerate(files.top_files(model_path)):
         out_i="%s/nn%d"%(out_path,i)
-        single.extract_frame_feats(frame_path,in_i,out_i)
+        fun(frame_path,in_i,out_i)
 
 def ts_feats(seqs_path,out_path=None):
     if(not out_path):
@@ -19,7 +21,7 @@ def ts_feats(seqs_path,out_path=None):
         out_i="%s/nn%d"%(out_path,i)
         single.compute_ts_feats(in_i,out_i)
 
-frame_path='../../sim3/time'
-model_path='../../sim3/ens3/models'
-#frame_feats(frame_path,model_path)
-ts_feats('../../sim3/ens3/seqs')
+frame_path='../MSR/ens1/time'
+model_path='../MSR/ens1/models'
+frame_feats(frame_path,model_path,fun=basic.extract)
+ts_feats('../MSR/ens1/seqs')
