7434593d23b544e91818cf11c6e8f7ec0889bcb8
tjacek <tjacek@agh.edu.pl>
2020-04-16 18:19:10 +0200

    sim module

--- frames/__init__.py
+++ frames/__init__.py
@@ -5,38 +5,34 @@ from keras.layers import Input,Add,Dense, Dropout, Flatten,BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D,ZeroPadding2D,Activation,Lambda
 from keras import regularizers
 import imgs,data,gen,single
-import ts.models
+import sim#ts.models
 from keras.models import load_model
 
-def extract(frame_path,model_path,out_path):
-    frames=imgs.read_frames(frame_path,as_dict=True)
-    X=data.format_frames(list(frames.values()))
-    names=list(frames.keys())
-    extractor=load_model(model_path)
-    X_feats=extractor.predict(X)
-    feat_dict={ names[i]:feat_i for i,feat_i in enumerate(X_feats)}
-    single.save_ts_feats(feat_dict,out_path)
+#def extract(frame_path,model_path,out_path):
+#    frames=imgs.read_frames(frame_path,as_dict=True)
+#    X=data.format_frames(list(frames.values()))
+#    names=list(frames.keys())
+#    extractor=load_model(model_path)
+#    X_feats=extractor.predict(X)
+#    feat_dict={ names[i]:feat_i for i,feat_i in enumerate(X_feats)}
+#    single.save_ts_feats(feat_dict,out_path)
 
 def make_model(in_path,out_path=None,n_epochs=5):
-    frames=imgs.read_frames(in_path,as_dict=True)
+    frames=imgs.read_seqs(in_path)
     train,test=data.split_dict(frames)
-    X=data.format_frames(list(train.values()))
-    y=[ int(name_i.split("_")[0])-1 for name_i in train.keys()]
-    X,y=gen.full_data(X,y)
+    X,y=data.to_seq_dataset(train)
+#    y=[ int(name_i.split("_")[0])-1 for name_i in train.keys()]
+    X,y=gen.binary_data(X,y)
+    print(len(y))
     n_channels=X[0].shape[-1]
-    sim_metric,model=make_five(20,n_channels,params=None)
+    params={"input_shape":(64,64,n_channels)} 
+    sim_metric,model=sim.build_siamese(params,make_five)
+#    make_five(20,n_channels,params=None)
     sim_metric.fit(X,y,epochs=n_epochs,batch_size=100)
     if(out_path):
         model.save(out_path)
 
-def make_five(n_cats,n_channels,params=None):
-    if(not params):
-        params={}
-    input_shape=(64,64,n_channels)
-    left_input = Input(input_shape)
-    right_input = Input(input_shape)
-
-    model = Sequential()
+def make_five(model):
     activ='relu'
     kern_size,pool_size,filters=(3,3),(2,2),[32,16,16,16]
     for filtr_i in filters:
@@ -44,15 +40,4 @@ def make_five(n_cats,n_channels,params=None):
         model.add(MaxPooling2D(pool_size, padding='same'))
     model.add(Flatten())
     model.add(Dense(64, activation=activ,name='hidden',kernel_regularizer=regularizers.l1(0.01)))
-
-    encoded_l = model(left_input)
-    encoded_r = model(right_input)
-
-    prediction,loss=ts.models.contr_loss(encoded_l,encoded_r)
-    siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
-    optimizer = keras.optimizers.Adam(lr = 0.00006)#keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True)
-    siamese_net.compile(loss=loss,#"binary_crossentropy",
-        optimizer=optimizer)
-    extractor=Model(inputs=model.get_input_at(0),outputs=model.get_layer("hidden").output)
-    extractor.summary()
-    return siamese_net,extractor
+    return model
