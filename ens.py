15fbcde0302d3432b4e8156feea9d26795778fda
tjacek <tjacek@student.agh.edu.pl>
2020-04-24 19:30:45 +0200

    fix bug

--- ens.py
+++ ens.py
@@ -10,8 +10,8 @@ def train_template(fun,in_path,out_path,n_epochs=5):
         fun(in_path,out_i,n_epochs,i)
 
 def extract_template(fun,frame_path,model_path,out_path):
-    if(not is_ens( in_path)):
-        return fun(in_path,out_path,n_epochs)
+    if(not is_ens( model_path)):
+        return fun(frame_path,model_path,out_path)
     files.make_dir(out_path)
     for in_i in files.top_files(frame_path):
         model_i='%s/%s'%(model_path,in_i.split("/")[-1])
@@ -20,7 +20,6 @@ def extract_template(fun,frame_path,model_path,out_path):
         fun(in_i,model_i,out_i)
 
 def transform_template(fun,in_path,out_path,info=None):
- #   raise Exception(in_path)
     if(not is_ens( in_path)):
         return fun(in_path,out_path,info)
     files.make_dir(out_path)
@@ -32,25 +31,7 @@ def transform_template(fun,in_path,out_path,info=None):
             fun(in_i,out_i)
 
 def is_ens(in_path):
+    if(not os.path.isdir(in_path)):
+        return False
     return any([  os.path.isdir(file_i)  
                     for file_i in  files.top_files(in_path)] )
-
-#def frame_feats(frame_path,model_path,out_path=None,fun=None):
-#    if(not out_path):
-#        dst_dir=os.path.split(model_path)[0]
-#        out_path=dst_dir+'/seqs'	
-#    files.make_dir(out_path)
-#    if(not fun):
-#        fun= single.extract_frame_feats
-#    for i,in_i in enumerate(files.top_files(model_path)):
-#        out_i="%s/nn%d"%(out_path,i)
-#        fun(frame_path,in_i,out_i)
-
-#def ts_feats(seqs_path,out_path=None):
-#    if(not out_path):
-#        dst_dir=os.path.split(seqs_path)[0]
-#        out_path=dst_dir+'/feats'	
-#    files.make_dir(out_path)
-#    for i,in_i in enumerate(files.top_files(seqs_path)):
-#        out_i="%s/nn%d"%(out_path,i)
-#        single.compute_ts_feats(in_i,out_i)
