370f88bc620b89335b93d9f4c17d3caf02f4ee4f
tjacek <tjacek@agh.edu.pl>
2020-05-07 21:00:21 +0200

    refactore exp module

--- exp.py
+++ exp.py
@@ -7,77 +7,59 @@ import files,stats,spline,agum
 import basic,basic.ts,ts,frames.ae
 
 def stats_feats(in_path):
-    dir_path=os.path.dirname(in_path)
-    dir_path+="/stats"
-    files.make_dir(dir_path)
-    stats.ens_stats(in_path,dir_path+"/feats")
+    paths=prepare_dirs(in_path,"stats",["feats"])
+    stats.ens_stats(in_path,paths["feats"])
 
 def basic_feats(in_path,n_epochs=1000):
-    dir_path=os.path.dirname(in_path)
-    dir_path+="/basic"
-    files.make_dir(dir_path)
-    spline_path= dir_path+"/spline"
-    spline.ens_upsample(in_path,spline_path)
-    model_path= dir_path+"/models"
-    basic.ts.ens_train(spline_path,model_path,n_epochs)
-    feat_path=dir_path+"/feats"
-    basic.ts.ens_extract(spline_path,model_path,feat_path)
+    paths=prepare_dirs(in_path,"basic",["spline","models","feats"])
+    spline.ens_upsample(in_path,paths["spline"])
+    basic.ts.ens_train(paths["spline"],paths["models"],n_epochs)
+    basic.ts.ens_extract(paths["spline"],paths["models"],paths["feat"])
 
 def agum_feats(in_path,n_epochs=1000):
-    dir_path=os.path.dirname(in_path)
-    dir_path+="/agum"
-    files.make_dir(dir_path)
-    spline_path= dir_path+"/spline"
-    spline.ens_upsample(in_path,spline_path)
-    agum_path=dir_path+"/agum_seqs"
-    files.make_dir(agum_path)
-    agum.ens_agum(spline_path,agum_path)
-    model_path= dir_path+"/models"
-    basic.ts.ens_train(agum_path,model_path,n_epochs)
-    feat_path=dir_path+"/feats"
-    basic.ts.ens_extract(spline_path,model_path,feat_path)
+    paths=prepare_dirs(in_path,"agum",["spline","agum_seqs","models","feats"])
+    spline.ens_upsample(in_path,paths["spline"])
+    files.make_dir(paths["agum_seqs"])
+    agum.ens_agum(paths["spline"],paths["agum_seqs"])
+    basic.ts.ens_train(paths["agum_seqs"],paths["models"],n_epochs)
+    basic.ts.ens_extract(paths["spline"],paths["models"],paths["feats"])
 
 def sim_feats(in_path,n_epochs=350):
-    dir_path=os.path.dirname(in_path)
-    dir_path+="/sim"
-    files.make_dir(dir_path)
-    spline_path= dir_path+"/spline"
-    spline.ens_upsample(in_path,spline_path)
-    model_path= dir_path+"/models"
-    ts.ens_train(spline_path,model_path,n_epochs)
-    feat_path=dir_path+"/feats"
-    ts.ens_extract(spline_path,model_path,feat_path)
+    paths=prepare_dirs(in_path,"sim",["spline","models","feats"])
+    spline.ens_upsample(in_path,paths["spline"])
+    ts.ens_train(paths["spline"],paths["models"],n_epochs)
+    ts.ens_extract(paths["spline"],paths["models"],paths["feats"])
 
 def ae_seqs(in_path,n_epochs=1000):
-    dir_path=os.path.dirname(in_path)
-    ae_path=dir_path+  "/ae"
-    seq_path=dir_path+ "/seqs"
-    frames.ae.make_model(in_path,ae_path,n_epochs)
-    frames.ae.extract(in_path,ae_path,seq_path)
+    paths=prepare_dirs(in_path,None,["ae","seqs"])
+    frames.ae.make_model(in_path,paths["ae"],n_epochs)
+    frames.ae.extract(in_path,paths["ae"],paths["seqs"])
 
 def sim_seqs(in_path,n_epochs=350):
-    dir_path=os.path.dirname(in_path)
-    model_path=dir_path+  "/frame_models"
-    seq_path=dir_path +"/seqs"
-    frames.ens_train(in_path,model_path,n_epochs)
-    frames.ens_extract(in_path,model_path,seq_path)
+    frames.ens_train(in_path,paths["frame_models"],n_epochs)
+    frames.ens_extract(in_path,paths["frame_models"],paths["seqs"])
 
 def basic_seqs(in_path,n_epochs=1000):
-    dir_path=os.path.dirname(in_path)
-    model_path=dir_path+  "/frame_models"
-    seq_path=dir_path +"/seqs"
-    basic.ens_train(in_path,model_path,n_epochs)
-    basic.ens_extract(in_path,model_path,seq_path)
+    paths=prepare_dirs(in_path,None,["frame_models","seqs"])
+    basic.ens_train(in_path,paths["frame_models"],n_epochs)
+    basic.ens_extract(in_path,paths["frame_models"],paths["seqs"])
 
 def full_seqs(in_path,n_epochs=1000):
-    dir_path=os.path.dirname(in_path)
-    model_path=dir_path+  "/frame_model"
-    basic.full_train(in_path,model_path,n_epochs)
-    seq_path=dir_path +"/seqs"
-    files.make_dir(seq_path)
+    paths=prepare_dirs(in_path,None,["frame_model","seqs"])
+    basic.full_train(in_path,paths["frame_model"],n_epochs)
+    files.make_dir(path["seqs"])
     basic.extract(model_path,seq_path,in_path)
 
+def prepare_dirs(in_path,name,sufixes):
+    dir_path=os.path.dirname(in_path)
+    if(name):
+        dir_path="%s/%s" %(dir_path,name)
+        files.make_dir(dir_path)
+    return get_paths(dir_path,sufixes)  
+
+def get_paths(dir_path,sufixes):
+    return {sufix_i:"%s/%s"%(dir_path,sufix_i) for sufix_i in sufixes }
 
 #ae_seqs("../proj2/full",n_epochs=1500)
 #basic_seqs("../ens6/tmp",1000)
-agum_feats("../proj2/seqs",1000)
+sim_feats("sim/full",5)
