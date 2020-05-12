a33ae367e3d04fa52e5f70706a5fdd806554b186
tjacek <tjacek@student.agh.edu.pl>
2020-05-12 22:52:50 +0200

    ordered frames

--- gen/frames.py
+++ gen/frames.py
@@ -1,4 +1,8 @@
 import random
+import gen
+
+def squared_from_pairs(X_pairs,y_pairs,n_frames):
+    return from_pairs_template(X_pairs,y_pairs,n_frames,squared_pairs)
 
 def ordered_from_pairs(X_pairs,y_pairs,n_frames):
     return from_pairs_template(X_pairs,y_pairs,n_frames,ordered_frames)
@@ -32,5 +36,15 @@ def ordered_frames(seq_a,seq_b,y_i,n_frames):
     X,y=[],[]
     for a_j,b_j in zip(a,b):
         X.append((seq_a[a_j],seq_b[b_j]))
+        y.append(y_i)
+    return X,y
+
+def squared_pairs(seq_a,seq_b,y_i,n_frames):
+#    raise Exception(n_frames)	
+    sample_i,sample_j=gen.get_sample(seq_a),gen.get_sample(seq_b)
+    frames_i,frames_j= sample_i(n_frames),sample_j(n_frames)   
+    X,y=[],[]
+    for k,t in zip(frames_i,frames_j):
+        X.append((seq_a[k],seq_b[t]))
         y.append(y_i)
     return X,y
