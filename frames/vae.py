2d78d1a090918a8990eb0e5cb57da6e29e166658
tjacek <tjacek@student.agh.edu.pl>
2020-06-03 22:10:39 +0200

    variational autoencoder

--- frames/vae.py
+++ frames/vae.py
@@ -1,4 +1,7 @@
-from keras.layers import Input, Dense,Conv2D,Reshape,Conv2DTranspose,Lambda
+from keras.layers import Input,Dense,Reshape,Lambda
+from keras.layers import Conv2D,Conv2DTranspose
+from keras.layers import Flatten,MaxPooling2D,UpSampling2D
+from keras.losses import mse
 from keras.models import Model
 from keras import backend as K
 import numpy as np
@@ -19,32 +22,58 @@ def make_model(in_path,out_path=None,n_epochs=1000,recon=True):
     X=np.array(X)
 #    add_noise(X)
     params={'n_channels':X.shape[-1]}
-    model,auto=make_autoencoder(params)
+    model=make_autoencoder(params)
+
+    original_dim = 64 * 64
+
     model.summary()
-    model.fit(X,X,epochs=n_epochs,batch_size=64)
-    auto.save(out_path)
-    if(recon):
-        model.save(out_path+"_recon")
+    model.fit(X,epochs=n_epochs,batch_size=64)
+    model.save(out_path)
 
 def make_autoencoder(params):
-    input_img = (64, 64)#, params['n_channels'])
-    intermediate_dim = 512
-    latent_dim = 64
-    original_dim = 64 * 64
+    input_img = (64, 64, params['n_channels'])
     inputs = Input(shape=input_img, name='encoder_input')
-    x = Dense(intermediate_dim, activation='relu')(inputs)
-    z_mean = Dense(latent_dim, name='z_mean')(x)
-    z_log_var = Dense(latent_dim, name='z_log_var')(x)
-    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
-    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-#    encoder.summary()
-    latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-    x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-    outputs = Dense(original_dim, activation='sigmoid')(x)
+    n_kerns=32
+    x = Conv2D(32, (5, 5), activation='relu',padding='same')(inputs)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(16, (5, 5), activation='relu',padding='same')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(16, (5, 5), activation='relu',padding='same')(x)
+    x = MaxPooling2D((2, 2))(x)
+    shape = K.int_shape(x)
+
+    x=Flatten()(x)
+
+    z_mean = Dense(100, name='z_mean')(x)
+    z_log_var = Dense(100, name='z_log_var')(x)
+    z = Lambda(sampling, output_shape=(100,), name='z')([z_mean, z_log_var])
+
+    encoder = Model(inputs, [z_mean], name='encoder')
+    encoder.summary()
+    latent_inputs = Input(shape=(100,), name='z_sampling')
+    x = Dense(shape[1]*shape[2]*shape[3])(latent_inputs)
+    x = Reshape((shape[1], shape[2], shape[3]))(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2DTranspose(16, (5, 5), activation='relu',padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2DTranspose(16, (5, 5), activation='relu',padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2DTranspose(32, (5, 5), activation='relu',padding='same')(x)
+
+    outputs=Conv2DTranspose(filters=params['n_channels'],kernel_size=n_kerns,padding='same')(x)
     decoder = Model(latent_inputs, outputs, name='decoder')
-    
-    outputs = decoder(encoder(inputs)[2])
+    decoder.summary()
+
+    outputs = decoder(encoder(inputs))
     vae = Model(inputs, outputs, name='vae_mlp')
-    
     vae.summary()
-    raise Exception("OK")
+
+    reconstruction_loss = mse(inputs, outputs)
+#    reconstruction_loss *= (64*64)
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss) + K.mean(kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='adam')
+    return vae
