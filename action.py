7892bc4e6e78fdef03fd0d64e81a823e760e9d80
tjacek <tjacek@student.agh.edu.pl>
2020-08-14 16:24:42 +0200

    ens action

--- action.py
+++ action.py
@@ -4,8 +4,12 @@ def exp1(in_path,n_epochs=350):
 	paths=exp.get_paths(in_path,["frames","nn","feat"])
 	print(paths)
 	frames.action.make_model(paths["frames"],paths["nn"],n_epochs)
-	frames.action.extract(paths["frames"],paths["nn"],paths["feat"])
+	frames.action.extract(paths["nn"],paths["feat"],paths["frames"])
 
-#exp1("../MSR_exp1")
-#frames.action.ens_train("../MSR_exp1/exp1/frames","../MSR_exp1/exp1/models",n_epochs=350)
-frames.action.ens_extract("../MSR_exp1/exp1/frames","../MSR_exp1/exp1/ens/models","../MSR_exp1/exp1/ens/feats")
+def ens_exp(in_path):
+    paths=exp.get_paths(in_path,["frames","models","feats"])
+    frames.action.ens_train(paths["frames"],paths["models"],n_epochs=350)
+    frames.action.ens_extract(paths["frames"],paths["models"], paths["feats"])
+
+exp1("../MSR_exp1/exp5")
+#ens_exp("../MSR_exp1/exp2/")
