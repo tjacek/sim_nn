633e4a164f726963b87459f83ca0bc8d425cf55d
tjacek <tjacek@agh.edu.pl>
2020-10-19 13:13:53 +0200

    unify agum

--- agum.py
+++ agum.py
@@ -7,7 +7,26 @@ def simple_agum(in_path,ae_model):
             for path_i in ["frames","seqs"]}
     frames.ae.extract(paths["frames"],ae_model,paths["seqs"])
     exp.basic_feats(paths["seqs"])
+    exp.stats_feats(paths["seqs"])
 
+def unify_agum(paths,ae_model,out_path):
+    img_dict=[ imgs.read_seqs(path_i)
+                for path_i in paths]
+    agum_set=img_dict[0]
+    for i,dict_i in enumerate(img_dict[1:]):
+        for name_j,seq_j in dict_i.items():
+            if(in_train(name_j)):
+                name_j="%s_%d" % (name_j,i)
+                agum_set[name_j]=seq_j
+    files.make_dir(out_path)
+    seq_path="%s/%s" % (out_path,"frames")
+    imgs.save_seqs(agum_set,seq_path)
+    simple_agum(out_path,ae_model)
+
+def in_train(name_j):
+    name_j=files.clean_str(name_j)
+    person_j=int(name_j.split("_")[1])
+    return (person_j %2)==1
 #def img_agum(in_path,ae_model):
 #    final_seqs,paths=get_agum_data(in_path)
 #    frames.ae.extract(final_seqs,ae_model,paths["seqs"])
@@ -48,7 +67,6 @@ def simple_agum(in_path,ae_model):
 
 if __name__ == "__main__":
     ae_model="../common/ae"
-    simple_agum("../short",ae_model)
-#    img_agum("../agum/gap","../agum/l1/ae") 
-#ens_agum("../agum/gap","../ens5/frame_models")
-#exp.basic_feats("../agum/ens/seqs")
+#    simple_agum("../short",ae_model)
+    paths=["../full","../short/frames"]
+    unify_agum(paths,ae_model,"test")
